43a8a02244818fcf53a74f4c4423a56338d2dbf1
Ai Sohrabi <alisohrabit@gmail.com>
2019-11-24 13:40:05 +0330

    Add files via upload

--- assets/js/acf4ele.js
+++ assets/js/acf4ele.js
@@ -3,12 +3,12 @@
  	 * @param $scope The Widget wrapper element as a jQuery element
 	 * @param $ The jQuery alias
 	 */ 
-	var WidgetHelloWorldHandler = function( $scope, $ ) {
+	var WidgetACF4EleHandler = function( $scope, $ ) {
 		console.log( $scope );
 	};
 	
 	// Make sure you run this code under Elementor.
 	$( window ).on( 'elementor/frontend/init', function() {
-		elementorFrontend.hooks.addAction( 'frontend/element_ready/hello-world.default', WidgetHelloWorldHandler );
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/ACF4Ele.default', WidgetACF4EleHandler );
 	} );
 } )( jQuery );
